fcf2546468f631c15b2140c7595e36257bf40aed
rjameel <90274183+rjameel@users.noreply.github.com>
2021-11-24 15:28:38 -0600

    Add files via upload

---
+++ demofile.py
@@ -0,0 +1,4 @@
+from seleniumbase import BaseCase
+class demo_azurepipeline(BaseCase):
+    def test_azurepipeline(self):
+        self.open("https://www.tst.apartmenthomeliving.com/atlanta-ga/homes-for-rent/")
---
+++ demofile2.py
@@ -0,0 +1,5 @@
+from seleniumbase import BaseCase
+class demo_azurepipeline(BaseCase):
+    def test_azurepipeline(self):
+        self.open("https://www.tst.apartmenthomeliving.com/")
+        self.type("#ss","Austin")
